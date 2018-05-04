a89a83fc615511e1661a036b83dbca45f0c2eb8c
ShuTsing <shutsing@outlook.com>
2018-05-04 10:13:27 +0800

    up

--- Renderer3D/GraLib.cpp
+++ Renderer3D/GraLib.cpp
@@ -12,4 +12,16 @@ namespace gra
 		}
 		return;
 	}
+
+	void MemsetWord(void* dest, unsigned int val, int cnt)
+	{
+		_asm
+		{
+			mov edi, dest;
+			mov ecx, cnt;
+			mov ax, val;
+			rep stosw;
+		}
+		return;
+	}
 }
